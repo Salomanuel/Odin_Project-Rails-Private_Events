ac3726f5d8e30e773d7b17395672b29ec1282a0a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-10 18:43:44 +0200

    setup seeds with Pokemon generators!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Manuel")
+
+30.times do 
+	name = Faker::Pokemon.name
+	User.create!(name: name)
+end
+
+Event.create!(name: "Drunk Jenga",
+							date: Time.now,
+							creator_id: 1)
+
+10.times do
+	name  = "Party in #{Faker::Pokemon.location}"
+	date  = Faker::Date.between(1.year.ago, 1.year.from_now)
+	owner = rand(User.all.count) + 1
+	Event.create!(name: name,
+								date: date,
+								creator_id: owner)
+end
