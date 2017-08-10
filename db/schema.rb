c4c73330af39c5bd2371e059c7ec9e73d43e0aee
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-10 18:02:14 +0200

    add relationships User(creator)=>Event and also add creator_id to Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809150240) do
+ActiveRecord::Schema.define(version: 20170810160031) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
