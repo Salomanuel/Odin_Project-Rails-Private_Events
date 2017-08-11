8370c1bb606acae22df924cd50b8450f2a7ece48
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-11 17:17:05 +0200

    improve the Event index table

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,18 @@ class EventsController<ApplicationController
 	def show
 		@event = Event.find_by(id:params[:id])
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = current_user.created_events.create(events_params)
+		redirect_to @event
+	end
+
+	private
+		def events_params
+			params.require(:event).permit(:name, :date)
+		end
 end
