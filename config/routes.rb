b1f73082588bbaabc784895e91c83dd030e039f7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-11 11:53:37 +0200

    add routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
 	resources :users, 		only: [:show, :new, :create]
 	resources :sessions, 	only: [:new, :create, :destroy]
+	resources :events,		only: [:new, :create, :index, :show]
 
 	get  		'signup', to: 'users#new'
 	post 		'signup', to: 'users#create'
