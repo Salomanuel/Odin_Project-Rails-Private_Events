fd5e13ee9d4aee474215952c4272d3297869d634
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-11 16:10:24 +0200

    add show view for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,8 @@ class EventsController<ApplicationController
 		@second_column_title  = "created by"
 		@proc = Proc.new { |arg| arg.creator.name }
 	end
+
+	def show
+		@event = Event.find_by(id:params[:id])
+	end
 end
