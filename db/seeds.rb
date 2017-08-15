eaa10a1998de1c71a31b0db8b02b30eadf141b77
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-15 18:11:30 +0200

    add the Invitations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,33 @@
-User.create!(name: "Manuel")
+# MANUEL User, Manuel Party
+m = User.create!(name: "Manuel")
 
+e = Event.create!(name: "Drunk Jenga",
+							date: Time.now,
+							creator_id: 1)
+Invitation.create!( attendee_id: m.id, attended_event_id: e.id)
+
+# USERS
 30.times do 
 	name = Faker::Pokemon.name
 	User.create!(name: name)
 end
 
-Event.create!(name: "Drunk Jenga",
-							date: Time.now,
-							creator_id: 1)
-
+# EVENTS
 10.times do
 	name  = "Party in #{Faker::Pokemon.location}"
 	date  = Faker::Date.between(1.year.ago, 1.year.from_now)
 	owner = rand(User.all.count) + 1
-	Event.create!(name: name,
+	event = Event.create!(name: name,
 								date: date,
 								creator_id: owner)
+	Invitation.create!(
+		attendee_id: 				owner, 
+		attended_event_id: 	event.id )
+end
+
+# INVITATIONS
+30.times do
+	Invitation.create!(
+		attendee_id: 				rand(User.count) +1, 
+		attended_event_id: 	rand(Event.count)+1 )
 end
